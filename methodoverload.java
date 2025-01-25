d8c4aa54e89665a38d5ca7813c40fe847c99ad8b
pradeep435 <pradeep2005ravi@gmail.com>
2025-01-25 08:49:39 +0530

    Add files via upload

---
+++ methodoverload.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class methodoverload
+{
+int add(int a, int b)
+{
+return a+b;
+}
+int add (int a,int b ,int c)
+{
+return (a+b)*c;
+}
+public static void main(String args[])
+{
+Scanner Scan = new Scanner(System.in);
+methodoverload method = new methodoverload();
+System.out.println("enter a, b and c");
+int x = Scan.nextInt();
+int y = Scan.nextInt();
+int z = Scan.nextInt();
+System.out.println("sum:"+method.add(x,y,z));
+}
+}
